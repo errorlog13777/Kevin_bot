a88a0039aca065f2ad28abc2fe8d47ba7bad82c6
DF <starbfly10377.com>
2020-04-30 21:07:11 +0800

    update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,5 +1,6 @@
 import discord
 import json
+import datetime
 from discord.ext import commands
 from core.classes import Cog_Extension
 
@@ -22,5 +23,24 @@ class Main(Cog_Extension):  # Main 繼承 commands.Cog 裡面的所有屬性、�
     async def hi(self, ctx):
         await ctx.send(ctx.message.author.mention + f' 哈囉，你好這裡是台灣，我是台灣人 Kevin 張凱文，下面這個人是我的女朋友陳鮭魚')
         await ctx.send(jdata['salmon'])
+
+    @commands.command()
+    async def em(self, ctx):    # embed 生成器 https://cog-creators.github.io/discord-embed-sandbox/
+        embed=discord.Embed(title="Kevin Bot", url="https://www.instagram.com/ke_.y.c/", description="Just a Kevin Bot", color=0x00ff40, timestamp=datetime.datetime.now() - datetime.timedelta(hours = 8))
+        embed.set_author(name="Kevin fans", url="https://www.instagram.com/ke_.y.c/")
+        embed.set_thumbnail(url="https://scontent-frt3-2.cdninstagram.com/v/t51.2885-19/91286268_679708962764318_6840888535994597376_n.jpg?_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_ohc=fIe4jC20CCEAX_2IZG1&oh=86441324bde90181657aadbd83680dc2&oe=5ED2922F")
+        embed.add_field(name="website", value="https://www.instagram.com/ke_.y.c/", inline=False)
+        embed.set_footer(text="高科電神張凱文")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def sayd(self, ctx, *, msg):  # 刪除使用者訊息，由機器人複誦 (* 代表在此之後的參數不論有多少都會作為 msg 的 argument, msg 為使用者訊息參數)
+        await ctx.message.delete()
+        await ctx.send(msg)
+
+    @commands.command()
+    async def purge(self, ctx, num: int):  # num : int 的意思代表先註解 argument 的資料型態
+        await ctx.channel.purge(limit = num + 1)    # +1 的作用為多刪除打指令的訊息
+
 def setup(bot):             # 機器人執行時會自動呼叫 setup, bot 為 bot.py 內的實體 bot
     bot.add_cog(Main(bot))  # bot.add_cog() 呼叫 main.py 的 Main 並傳入參數 bot
